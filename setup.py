95ea2ddd6de6f11d00b1e9e295af47c7486dc873
williamfzc <178894043@qq.com>
2020-10-18 17:26:24 +0800

    release: 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 __PROJECT_NAME__ = r"cobertura_parser"
 __AUTHOR__ = r"williamfzc"
 __AUTHOR_EMAIL__ = r"fengzc@vip.qq.com"
-__VERSION__ = r"0.1.2"
+__VERSION__ = r"0.2.0"
 
 setup(
     name=__PROJECT_NAME__,
