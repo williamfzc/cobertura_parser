20e6b5c307136e84960d4c07c54498f2c08e65fe
williamfzc <178894043@qq.com>
2021-01-29 00:06:40 +0800

    feat: cli

---
+++ cobertura_parser/cli.py
@@ -0,0 +1,52 @@
+import fire
+
+from cobertura_parser.loader import CoberturaLoader
+from cobertura_parser.models.builtin import CoberturaStructure
+from cobertura_parser.processor import CoberturaProcessor
+
+
+class TerminalCli(object):
+    def cov(self, from_file: str, to_file: str):
+        structure: CoberturaStructure = CoberturaLoader.from_file(from_file)
+        result = CoberturaProcessor.get_coverage(structure)
+        with open(to_file, "w") as f:
+            f.write(result.json(exclude={
+                "sources": ...,
+                "line_rate": ...,
+                "branch_rate": ...,
+                "line_covered": ...,
+                "line_valid": ...,
+                "branches_covered": ...,
+                "branches_valid": ...,
+                "complexity": ...,
+                "version": ...,
+                "packages": {
+                    "__all__": {
+                        "classes": {
+                            "__all__": {
+                                "methods": {
+                                    "__all__": {
+                                        "branch_rate",
+                                        "line_rate",
+                                        "complexity",
+                                    }
+                                },
+                                "branch_rate": ...,
+                                "line_rate": ...,
+                                "complexity": ...,
+                            }
+                        },
+                        "branch_rate": ...,
+                        "line_rate": ...,
+                        "complexity": ...,
+                    }
+                }
+            }))
+
+
+def main():
+    fire.Fire(TerminalCli)
+
+
+if __name__ == "__main__":
+    main()
