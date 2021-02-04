ccd9c50c517a3ac2c666216246c0741e20483918
williamfzc <178894043@qq.com>
2021-02-04 22:32:33 +0800

    feat: jacoco xml -> cobertura xml

--- cobertura_parser/cli.py
+++ cobertura_parser/cli.py
@@ -4,6 +4,7 @@ from cobertura_parser.loader import CoberturaLoader
 from cobertura_parser.models.builtin import CoberturaStructure
 from cobertura_parser.processor import CoberturaProcessor
 from cobertura_parser.utils import time_measure
+from cobertura_parser.ext.jacoco import jacoco2cobertura
 
 
 class TerminalCli(object):
@@ -57,6 +58,11 @@ class TerminalCli(object):
             structure: CoberturaStructure = CoberturaLoader.from_jacoco_file(from_file)
             with open(to_file, "w") as f:
                 f.write(self._cov(structure))
+
+    def xml_from_jacoco(self, from_file: str, to_file: str, dev: bool = None):
+        with time_measure("xml_from_jacoco", dev):
+            with open(to_file, "w") as f:
+                f.write(jacoco2cobertura(from_file))
 
 
 def main():
